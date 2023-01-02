9daab56e0bb09e8a0af3fff2be3e852165dc2cbe
Vazgen Torosyan <vazgen.torosyan@railsbank.com>
2023-01-02 23:08:03 +0400

    added output

--- .github/actions/build-s3-javascript/main.js
+++ .github/actions/build-s3-javascript/main.js
@@ -12,7 +12,9 @@ function run() {
     const s3Uri = `s3://${bucketName}`;
     exec.exec(`aws s3 sync ${artifactsFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello from my Javascript action!');
+    const websiteUrl = `http://${bucketName}.s3.amazonaws.com/index.html`
+
+    core.setOutput('website-url', websiteUrl)
 }
 
 run();
