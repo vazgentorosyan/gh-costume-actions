f6b44e13eea7f2e9ee2febe7e95197d2ab73e0cb
Vazgen Torosyan <vazgen.torosyan@railsbank.com>
2023-01-02 22:41:30 +0400

    added js costume action uploading site to s3

--- .github/actions/build-s3-javascript/main.js
+++ .github/actions/build-s3-javascript/main.js
@@ -3,6 +3,15 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // get input values
+    const bucketName = core.getInput('bucket-name', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const artifactsFolder = core.getInput('artifacts-folder', { required: true });
+
+    // Upload files to the S3
+    const s3Uri = `s3://${bucketName}`;
+    exec.exec(`aws s3 sync ${artifactsFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Hello from my Javascript action!');
 }
 
